538e1b609490ebe85171471b3405e80fb3ad732a
unknown <au549312@uni.au.dk>
2019-09-02 10:28:20 +0200

    simple calculator program, without test

---
+++ calculator/calculator/Functions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace calculator
+{
+    class Functions
+    {
+        public double Add(double a, double b)
+        {
+            double c = a + b;
+            Console.WriteLine("Adding: " + a + " + " + b + " = " + c);
+
+            return c;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            double c = a - b;
+            Console.WriteLine("Subtracting: " + a + " - " + b + " = " + c);
+
+            return c;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            double c = a * b;
+            Console.WriteLine("Multiplying: " + a + " * " + b + " = " + c);
+
+            return c;
+        }
+
+        public double Power(double x, double exp)
+        {
+            double c = Math.Pow(x, exp);
+
+            Console.WriteLine("Power: " + x + " ^ " + exp + " = " + c);
+
+            return c;
+        }
+    }
+}
---
+++ calculator/calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Functions fnc = new Functions();
+
+            Console.WriteLine("*** Welcome to calculator! ***");
+
+            fnc.Add(3, 4);
+
+        }
+    }
+}
