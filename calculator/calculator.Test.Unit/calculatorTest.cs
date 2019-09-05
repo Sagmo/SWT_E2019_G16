e1ccda1dac47aa6279b2e1879149f02953bbcfa9
Sivert Sømme Sagmo <au549312@uni.au.dk>
2019-09-05 14:46:40 +0200

    small modification to other calculator test

---
+++ calculator/calculator.Test.Unit/calculatorTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace calculator.Test.Unit
+{
+    [TestFixture]
+    public class calculatorTest
+    {
+        [Test]
+        public void Add_AddNumbers_Return()
+        {
+
+        }
+    }
+}
