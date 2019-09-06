90d4891444a1871b1340df3b9b5d83ecbdabc93e
Glenn Laursen <201703930@post.au.dk>
2019-09-06 10:31:17 +0200

    Made overload functions

--- UnitTestingCalculatorSolution/Calculator/Calculator.cs
+++ UnitTestingCalculatorSolution/Calculator/Calculator.cs
@@ -55,27 +55,32 @@ namespace Calculator
 
         public double Add(double addend)
         {
-
+            Accumulator += addend;
+            return Accumulator;
         }
 
         public double Subtract(double subtractor)
         {
-
+            Accumulator -= subtractor;
+            return Accumulator;
         }
 
         public double Multiply(double multiplier)
         {
-
+            Accumulator *= multiplier;
+            return Accumulator;
         }
 
         public double Divide(double divisor)
         {
-
+            Accumulator /= divisor;
+            return Accumulator;
         }
 
         public double Power(double exponent)
         {
-
+            Accumulator = Math.Pow(Accumulator, exponent);
+            return Accumulator;
         }
 
     }
